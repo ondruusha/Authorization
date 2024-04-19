218375d061d0be2efe9b8221aae2291dd20cf18c
Andrey <105154073+ondruusha@users.noreply.github.com>
2024-04-19 13:54:36 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,23 @@
-function checkCredentials() {
-    var login = document.querySelector('input[type="login"]').value;
-    var password = document.querySelector('input[type="password"]').value;
+function page1auth() {
+    let login = document.querySelector('input[type="login"]').value;
+    let password = document.querySelector('input[type="password"]').value;
     if (login === 'login' && password === 'password') {
-        window.location.href = 'https://sdo.ket44.ru';
+        
+        console.log('Вход произведен успешно!');
     } else {
         alert('Неверный логин или пароль. Попробуйте еще раз.');
+        console.log('Зарегистрируйтесь!');
     }
 }
-function Proverka() {
-    if (login === 'login' && password === 'password') {
-    }
-    console.log('Вход произведен успешно!')
-}
-function registr() {
-    window.location.href = 'registration.html';
-}
-function gotovo() {
-    window.location.href = 'index.html';
+function Registered() {
+    window.location.href = 'registration.html'
+    let registeredUsers = {};
+    document.getElementById('login.registr').addEventListener('submit', function (event) {
+        event.preventDefault();
+        let newUsername = document.getElementById('newUsername').value;
+        let newPassword = document.getElementById('newPassword').value;
+        registeredUsers[newUsername] = newPassword;
+        alert('Registered new user: ' + newUsername);
+    })
+    window.location.href = 'index.html';;
 }
