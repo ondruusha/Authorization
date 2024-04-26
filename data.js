e506a3147318688c1685b184cea2224e126cc147
Andrey <105154073+ondruusha@users.noreply.github.com>
2024-04-26 12:24:26 +0300

    Update data.js

--- data.js
+++ data.js
@@ -4,11 +4,9 @@ function registerUser(login, password) {
         alert('Пользователь с таким логином уже существует.');
         return;
     }
-
     // Храним данные пользователя в localStorage
     // В реальном приложении здесь должна быть реализация хеширования пароля
     localStorage.setItem(login, password);
-
     alert('Пользователь зарегистрирован: ' + login);
 }
 
@@ -16,13 +14,13 @@ function registerUser(login, password) {
 function authenticateUser(login, password) {
     // Получаем сохраненный пароль
     var storedPassword = localStorage.getItem(login);
-
     // Проверяем, совпадает ли введенный пароль с сохраненным
     if (storedPassword === password) {
-        alert('Авторизация прошла успешно: ' + login);
-        return true;
+        alert('Авторизация прошла успешно!');
+        console.log('Авторизация прошла успешно!')
+        window.location.href = 'https://sdo.ket44.ru';
     } else {
-        alert('Ошибка авторизации: ' + login);
+        alert('Ошибка авторизации!');
         return false;
     }
 }
@@ -34,7 +32,6 @@ document.getElementById('registrationForm').addEventListener('submit', function 
     var password = document.getElementById('regPassword').value;
     registerUser(login, password);
 });
-
 document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
     var login = document.getElementById('login').value;
